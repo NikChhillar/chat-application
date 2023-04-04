db240d5342225e3f3e8cafb6f39f402a79ce566a
Nikhil <nikhilchhillar29@gmail.com>
2023-04-04 13:48:37 +0530

    frontend ui completed

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,39 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './Components/Dashboard';
 import Form from './Components/Form';
 
+// 
+const ProtectedRoutes = ({ children, auth = false }) => {
+  const isLoggedIn = localStorage.getItem('user:token') !== null || false;
+
+  if (!isLoggedIn && auth) {
+    return <Navigate to={'/users/sign_in'} />
+  } else if (isLoggedIn && ['/users/sign_in', '/users/sign_up'].includes(window.location.pathname)) {
+    console.log('object :>> ');
+    return <Navigate to={'/'} />
+  }
+
+  return children
+}
+
+// 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='/users/sign_in' element={<Form isSignInPage={true} />} />
-        <Route path='/users/sign_up' element={<Form isSignInPage={false} />} />
+        <Route path='/' element={
+          <ProtectedRoutes auth={true}>
+            <Dashboard />
+          </ProtectedRoutes>} />
+        <Route path='/users/sign_in' element={
+          <ProtectedRoutes>
+            <Form isSignInPage={true} />
+          </ProtectedRoutes>} />
+        <Route path='/users/sign_up' element={
+          <ProtectedRoutes>
+            <Form isSignInPage={false} />
+          </ProtectedRoutes>
+        } />
       </Routes>
     </BrowserRouter>
   );
--- client/src/Components/Form.jsx
+++ client/src/Components/Form.jsx
@@ -20,46 +20,52 @@ const Form = ({ isSignInPage = true }) => {
     <div className="bg-light h-screen flex items-center justify-center">
       <div className="bg-white w-[600px] h-[800px] shadow-lg flex flex-col justify-center items-center">
         <div className="text-4xl font-extrabold">
-          Welcome {isSignInPage ? "Back" : "Sir"}
+          Welcome {isSignInPage ? "Back" : "..."}
         </div>
         <div className="text-xl font-light font-serif mb-14">
           {!isSignInPage
             ? " Become one of us...."
             : "Prove that you are one of us...."}
         </div>
-        {!isSignInPage && (
+        <form
+          className="flex flex-col items-center w-full"
+          onSubmit={() => console.log("form submitted")}
+        >
+          {!isSignInPage && (
+            <Input
+              label="Your Name"
+              name="name"
+              placeholder="Name here..."
+              className="mb-6 w-[75%]"
+              value={data.fullName}
+              onChange={(e) => setData({ ...data, fullName: e.target.value })}
+            />
+          )}
+
           <Input
-            label="Your Name"
-            name="name"
-            placeholder="Name here..."
+            label="Email Address"
+            name="email"
+            type="email"
+            placeholder="Email here..."
             className="mb-6 w-[75%]"
-            value={data.fullName}
-            onChange={(e) => setData({ ...data, fullName: e.target.value })}
+            value={data.email}
+            onChange={(e) => setData({ ...data, email: e.target.value })}
           />
-        )}
-        <Input
-          label="Email Address"
-          name="email"
-          type="email"
-          placeholder="Email here..."
-          className="mb-6 w-[75%]"
-          value={data.email}
-          onChange={(e) => setData({ ...data, email: e.target.value })}
-        />
-        <Input
-          label="Password"
-          name="password"
-          type="password"
-          placeholder="Password here..."
-          className="mb-14 w-[75%]"
-          value={data.password}
-          onChange={(e) => setData({ ...data, password: e.target.value })}
-        />
-        <Button
-          label={isSignInPage ? "Log in" : "Sign up"}
-          type="submit"
-          className="w-[75%] mb-2"
-        />
+          <Input
+            label="Password"
+            name="password"
+            type="password"
+            placeholder="Password here..."
+            className="mb-14 w-[75%]"
+            value={data.password}
+            onChange={(e) => setData({ ...data, password: e.target.value })}
+          />
+          <Button
+            label={isSignInPage ? "Log in" : "Sign up"}
+            type="submit"
+            className="mb-2 w-[30%]"
+          />
+        </form>
         <div>
           {isSignInPage
             ? "Didn't have an account?..."
